5a5365bd6685d023b5d1f02150c41308214fdc1b
Oskar Niburski <oskarniburski@gmail.com>
2016-06-05 02:43:24 -0400

    some tests

--- tests/unit/models/friend-test.js
+++ tests/unit/models/friend-test.js
@@ -1,12 +1,19 @@
-import { moduleForModel, test } from 'ember-qunit';
-
-moduleForModel('friend', 'Unit | Model | friend', {
-  // Specify the other units that are required for this test.
-  needs: []
+import { test, moduleForModel } from 'ember-qunit'; import Ember from 'ember';
+moduleForModel('friend', 'Friend', {
+  needs: ['model:article']
+});
+test('it exists', function(assert) { var model = this.subject(); assert.ok(model);
+});
+test('fullName joins first and last name', function(assert) {
+var model = this.subject({firstName: 'Syd', lastName: 'Barrett'});
+  assert.equal(model.get('fullName'), 'Syd Barrett');
+Ember.run(function() { model.set('firstName', 'Geddy');
+});
+  assert.equal(model.get('fullName'), 'Geddy Barrett', 'Updates fullName');
 });
 
-test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
+test('articles relationship', function(assert) { var klass = this.subject({}).constructor;
+var relationship = Ember.get(klass, 'relationshipsByName').get('articles');
+  assert.equal(relationship.key, 'articles');
+  assert.equal(relationship.kind, 'hasMany');
 });
--- tests/unit/utils/date-helpers-test.js
+++ tests/unit/utils/date-helpers-test.js
@@ -1,10 +1,8 @@
-import dateHelpers from 'borrowers/utils/date-helpers';
-import { module, test } from 'qunit';
-
-module('Unit | Utility | date helpers');
-
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = dateHelpers();
-  assert.ok(result);
+import { module, test } from 'ember-qunit';
+import dateHelpers from '../../../utils/date-helpers';
+module('Utils: formatDate');
+test('formats a date object', function(assert) {
+var date = new Date("11-3-2015");
+var result = dateHelpers.formatDate(date, 'ddd MMM DD YYYY');
+  assert.equal(result, 'Mon Nov 03 2014', 'returns a readable string');
 });
