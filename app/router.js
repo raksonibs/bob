5b84bbd658282d41735fa4938b1ab29585382ec5
Oskar Niburski <oskarniburski@gmail.com>
2016-06-04 21:41:28 -0400

    Start of nested

--- app/router.js
+++ app/router.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+var Router = Ember.Router.extend({
   location: config.locationType
 });
 
@@ -11,6 +11,10 @@ Router.map(function() {
 
     this.route('show', {
       path: ':friend_id'
+    }, function() {
+      this.route('articles', {resetNamespace: true}, function() {
+        this.route('new');
+      });
     });
 
     this.route('edit', {
