bb1b10b42d8b305847c81eca1cd6013fb18089b9
Oskar Niburski <oskarniburski@gmail.com>
2016-06-11 23:37:09 -0400

    Adapter change

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,7 +1,18 @@
 import config from '../config/environment';
-import Firebase from 'firebase';
-import FirebaseAdapter from 'emberfire/adapters/firebase';
+// import Firebase from 'firebase';
+// import FirebaseAdapter from 'emberfire/adapters/firebase';
 
-export default FirebaseAdapter.extend({
-  firebase: new Firebase(config.firebase)
+// export default FirebaseAdapter.extend({
+//   firebase: new Firebase(config.firebase)
+// });
+
+import DS from 'ember-data';
+
+export default DS.RESTAdapter.extend({
+  host: 'http://localhost:3000',
+  namespace: 'api',
+  coalesceFindRequests: true,
+  headers: {
+      "Content-type": "x-www-form-urlencoded" // workaround for laravel
+  }
 });
