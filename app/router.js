c1c0071b17c994106eb09988063cf2ede2edf414
Oskar Niburski <oskarniburski@gmail.com>
2016-06-11 23:20:15 -0400

    Firebase adapter issue

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
+
+  this.route('admin', function() {
+    this.route('invitations');
+  });
 });
 
 export default Router;
