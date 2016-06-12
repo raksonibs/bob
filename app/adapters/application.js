390c8992576dde70edcc0e960aaef7a20633556b
Oskar Niburski <oskarniburski@gmail.com>
2016-06-12 12:42:33 -0400

    Setup math on insert element

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,15 @@
-import FirebaseAdapter from 'emberfire/adapters/firebase';
+// import FirebaseAdapter from 'emberfire/adapters/firebase';
 
-export default FirebaseAdapter.extend({
+// export default FirebaseAdapter.extend({
+// });
+
+import DS from 'ember-data';
+
+export default DS.RESTAdapter.extend({
+  host: 'http://localhost:3000',
+  namespace: 'api',
+  coalesceFindRequests: true,
+  headers: {
+      "Content-type": "x-www-form-urlencoded" // workaround for laravel
+  }
 });
