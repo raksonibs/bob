eb9985d9134a18651cb0fa027f2af0a9e6e6b0b3
Oskar Niburski <oskarniburski@gmail.com>
2016-06-19 12:37:39 -0400

    Initial materialize css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,6 +19,10 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-
+  // var materializeFonts = new Funnel('bower_components/materialize/font/roboto', {
+  //     srcDir: '/',
+  //     include: ['*.woff', '*.ttf', '*.woff2'],
+  //     destDir: '/font/roboto'
+  // });
   return app.toTree();
 };
