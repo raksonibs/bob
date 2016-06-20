5763f34c15d159cab0035bd512a5553e7081d9eb
Oskar Niburski <oskarniburski@gmail.com>
2016-06-20 00:06:06 -0400

    Article count + hover style

--- app/components/world-map.js
+++ app/components/world-map.js
@@ -1,4 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
+  antCount: Ember.computed(function() {
+    return this.get('store').query('location', { continent: "Antartica"})
+  }),
+  namCount: Ember.computed(function() {
+    return this.get('store').query('location', { continent: "North America"})
+  }),
+  samCount: Ember.computed(function() {
+    return this.get('store').query('location', { continent: "South America"})
+  }),
+  asiaCount: Ember.computed(function() {
+    return this.get('store').query('location', { continent: "Asia"})
+  }),
+  afrCount: Ember.computed(function() {
+    return this.get('store').query('location', { continent: "Africa"})
+  }),
+  eurCount: Ember.computed(function() {
+    return this.get('store').query('location', { continent: "Europe"})
+  }),
+  oceCount: Ember.computed(function() {
+    return this.get('store').query('location', { continent: "Oceania"})
+  })
 });
