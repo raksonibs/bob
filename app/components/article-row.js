d32cea1e9506bfa0c064c69069550e9884b5ff08
Oskar Niburski <oskarniburski@gmail.com>
2016-06-22 23:14:23 -0400

    user pass around

--- app/components/article-row.js
+++ app/components/article-row.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  authManager: Ember.inject.service('session'),
   actions: {
     blurBackground() {
       this.set('isModalOpen', true)
       this.get('blurBackground')(true)
+    },
+    favouriteBtn(article) {
+      this.get('favouriteBtnClick')(article)
     },
     close() {
       this.set('isModalOpen', false)
--- app/components/favourite-btn.js
+++ app/components/favourite-btn.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  authManager: Ember.inject.service('session'),
   actions: {
     favouriteBtn(article) {
       this.get('favouriteBtnClick')(article)
