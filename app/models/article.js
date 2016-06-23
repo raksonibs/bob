19634172bd428c4f89b4182423eabfad6033bff4
Oskar Niburski <oskarniburski@gmail.com>
2016-06-23 00:05:08 -0400

    image

--- app/models/article.js
+++ app/models/article.js
@@ -5,6 +5,7 @@ export default DS.Model.extend({
   users: DS.hasMany('user', { async: true }),
   location: DS.belongsTo('location', { async: true }),
   story: DS.attr('string'),
+  imageUrl: DS.attr('string'),
   title: DS.attr('string'),
   publishedAt: DS.attr('date')
 });
